d205df3bf51b07352d3abd7173935d05284a3a74
Sandor Van Wassenhove <sandorw@palantir.com>
2015-09-25 01:15:56 -0400

    Added HumanPlayer implementation

---
+++ src/test/java/com/github/sandorw/mocabogaso/players/HumanPlayerTest.java
@@ -0,0 +1,49 @@
+package com.github.sandorw.mocabogaso.players;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.sandorw.mocabogaso.games.defaults.DefaultGameMove;
+import com.github.sandorw.mocabogaso.games.test.SimpleTestGameState;
+
+/**
+ * Test cases for HumanPlayer
+ * 
+ * @author sandorw
+ */
+public class HumanPlayerTest {
+    HumanPlayer player;
+    SimpleTestGameState gameState;
+    
+    @Before
+    public void before() {
+        gameState = new SimpleTestGameState();
+    }
+    
+    @Test
+    public void chooseNextMoveValidMoveTest() {
+        InputStream inputStream = new ByteArrayInputStream("1".getBytes(Charset.forName("UTF-8")));
+        System.setIn(inputStream);
+        player = new HumanPlayer();
+        DefaultGameMove move = player.chooseNextMove(gameState);
+        assertEquals(move.getLocation(), 1);
+        System.setIn(System.in);
+    }
+    
+    @Test
+    public void chooseNextMoveInvalidMoveTest() {
+        InputStream inputStream = new ByteArrayInputStream("4 4 1".getBytes(Charset.forName("UTF-8")));
+        System.setIn(inputStream);
+        player = new HumanPlayer();
+        DefaultGameMove move = player.chooseNextMove(gameState);
+        assertEquals(move.getLocation(), 1);
+        System.setIn(System.in);
+    }
+
+}
