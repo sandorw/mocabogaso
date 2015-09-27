ed368889d36b6f4279ba648ca2274daf5b10c4b5
Sandor Van Wassenhove <sandorw@palantir.com>
2015-09-27 17:58:45 +0100

    Added TicTacToeGameState

---
+++ src/test/java/com/github/sandorw/mocabogaso/games/tictactoe/TicTacToeGameStateTest.java
@@ -0,0 +1,27 @@
+package com.github.sandorw.mocabogaso.games.tictactoe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.github.sandorw.mocabogaso.Game;
+import com.github.sandorw.mocabogaso.games.GameResult;
+import com.github.sandorw.mocabogaso.games.defaults.DefaultGameMove;
+import com.github.sandorw.mocabogaso.games.mnkgame.MNKGameState;
+import com.github.sandorw.mocabogaso.players.AIPlayerFactory;
+
+public class TicTacToeGameStateTest {
+
+    @Test
+    public void playFullGameTest() {
+        TicTacToeGameState gameState = new TicTacToeGameState();
+        Game<DefaultGameMove, MNKGameState> game = new Game<>(gameState);
+        game.addPlayer("X", AIPlayerFactory.getNewAIPlayer(gameState, 50));
+        game.addPlayer("O", AIPlayerFactory.getNewAIPlayer(gameState, 50));
+        game.playGame();
+        assertTrue(game.isGameOver());
+        GameResult gameResult = game.getGameResult();
+        assertTrue(gameResult.isTie());
+    }
+
+}
