6efe6be6246f9235e9f4b4fe526fcea2b8931e14
Sandor Van Wassenhove <sandorw@palantir.com>
2015-09-30 00:45:50 +0100

    Added ConnectX game implementation

---
+++ src/main/java/com/github/sandorw/mocabogaso/games/connectx/ConnectXGameState.java
@@ -0,0 +1,59 @@
+package com.github.sandorw.mocabogaso.games.connectx;
+
+import java.util.List;
+
+import com.github.sandorw.mocabogaso.games.GameState;
+import com.github.sandorw.mocabogaso.games.defaults.DefaultGameMove;
+import com.github.sandorw.mocabogaso.games.defaults.DefaultGameResult;
+import com.github.sandorw.mocabogaso.games.mnkgame.MNKGameState;
+import com.google.common.collect.Lists;
+
+public class ConnectXGameState extends MNKGameState {
+
+    public ConnectXGameState(int m, int n, int k) {
+        super(m, n, k);
+    }
+    
+    @Override
+    public GameState<DefaultGameMove, DefaultGameResult> getCopy() {
+        ConnectXGameState copy = new ConnectXGameState(numRows, numCols, goalNumInARow);
+        for (int i=0; i < numRows; ++i)
+            for (int j=0; j < numCols; ++j)
+                copy.boardLocation[i][j] = boardLocation[i][j];
+        copy.nextPlayerName = nextPlayerName;
+        copy.winningPlayerName = winningPlayerName;
+        return copy;
+    }
+    
+    @Override
+    public List<DefaultGameMove> getAllValidMoves() {
+        List<DefaultGameMove> moveList = Lists.newArrayList();
+        for (int j=0; j < numCols; ++j)
+            for (int i=0; i < numRows; ++i)
+                if (boardLocation[i][j] == BoardStatus.EMPTY) {
+                    moveList.add(new DefaultGameMove(nextPlayerName, i*numCols + j));
+                    break;
+                }
+        return moveList;
+    }
+    
+    @Override
+    public boolean isValidMove(DefaultGameMove move) {
+        int i = getRowNumber(move.getLocation());
+        int j = getColNumber(move.getLocation());
+        return super.isValidMove(move) && ((i == 0) || (boardLocation[i-1][j] != BoardStatus.EMPTY));
+    }
+
+    @Override
+    public DefaultGameMove getMoveFromString(String input) {
+        int colNumber = Integer.parseInt(input);
+        int rowNumber = 0;
+        for (int i=0; i < numRows; ++i)
+            if (boardLocation[i][colNumber] == BoardStatus.EMPTY) {
+                rowNumber = i;
+                break;
+            }
+        return new DefaultGameMove(nextPlayerName, rowNumber*numCols + colNumber);
+    }
+
+}
