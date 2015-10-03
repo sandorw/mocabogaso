db599b7ba2f15dd6c3a777bd4fc6d7a30556d7ca
Sandor Van Wassenhove <sandorw@palantir.com>
2015-10-03 21:07:27 +0100

    Refactored NodeResults and NodeResultsService
    Refactored to follow the new semantics of MCST and to use the new iterator

--- src/main/java/com/github/sandorw/mocabogaso/ai/mcts/defaults/DefaultNodeResultsService.java
+++ src/main/java/com/github/sandorw/mocabogaso/ai/mcts/defaults/DefaultNodeResultsService.java
@@ -1,11 +1,19 @@
 package com.github.sandorw.mocabogaso.ai.mcts.defaults;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.LinkedBlockingDeque;
+
 import com.github.sandorw.mocabogaso.ai.mcts.MonteCarloSearchTree;
 import com.github.sandorw.mocabogaso.ai.mcts.MonteCarloSearchTree.SearchTreeIterator;
 import com.github.sandorw.mocabogaso.ai.mcts.NodeResultsService;
 import com.github.sandorw.mocabogaso.games.GameMove;
 import com.github.sandorw.mocabogaso.games.GameResult;
 import com.github.sandorw.mocabogaso.games.GameState;
+import com.google.common.collect.Queues;
+import com.google.common.collect.Sets;
 
 /**
  * Default NodeResultsService that works with DefaultNodeResults.
@@ -15,17 +23,27 @@ import com.github.sandorw.mocabogaso.games.GameState;
 public final class DefaultNodeResultsService implements NodeResultsService<DefaultNodeResults> {
 
     @Override
-    public <GM extends GameMove, GS extends GameState<GM, ? extends GameResult>> DefaultNodeResults getNewNodeResults(
-            GM appliedMove, GS resultGameState) {
-        return new DefaultNodeResults();
+    public <GM extends GameMove, GS extends GameState<GM, ? extends GameResult>>
+            DefaultNodeResults getNewNodeResults(GS gameState) {
+        return new DefaultNodeResults(gameState);
     }
-
+    
     @Override
-    public <GM extends GameMove> void propagateGameResult(GameResult gameResult, SearchTreeIterator<GM> treeIterator) {
-        while (treeIterator.hasPrevious()) {
-            MonteCarloSearchTree<GM>.SearchTreeNode treeNode = treeIterator.previous();
-            treeNode.applyGameResultFromSimulation(gameResult);
+    public <GM extends GameMove> void propagateGameResult(GameResult gameResult, SearchTreeIterator<GM,DefaultNodeResults> treeIterator) {
+        Set<DefaultNodeResults> nodeResultsSet = Sets.newIdentityHashSet();
+        Deque<SearchTreeIterator<GM,DefaultNodeResults>> iteratorDeque = Queues.newArrayDeque();
+        iteratorDeque.push(treeIterator);
+        while (!iteratorDeque.isEmpty()) {
+            SearchTreeIterator<GM,DefaultNodeResults> iterator = iteratorDeque.pop();
+            if (nodeResultsSet.add(iterator.getCurrentNodeResults())) {
+                while (iterator.hasNextParent()) {
+                    iterator.advanceParentNode();
+                    iteratorDeque.push(iterator.getCurrentParentIterator());
+                }
+            }
+        }
+        for (DefaultNodeResults nodeResults : nodeResultsSet) {
+            nodeResults.applyGameResult(gameResult);
         }
     }
-
 }
