165098c55ace4d9dde230ec9583f14d116a5633e
Sandor Van Wassenhove <sandorw@palantir.com>
2015-10-03 19:49:02 +0100

    Refactor of MonteCarloSearchTree
    Work in progress, only MCST updated
    Preparation for Zobrist hashing and transposition tables
    Moves are now stored on edges between nodes
    Nodes now support having multiple parent nodes
    Iterator completely reworked for future AMAF support

--- src/main/java/com/github/sandorw/mocabogaso/ai/mcts/MonteCarloSearchTree.java
+++ src/main/java/com/github/sandorw/mocabogaso/ai/mcts/MonteCarloSearchTree.java
@@ -1,8 +1,8 @@
 package com.github.sandorw.mocabogaso.ai.mcts;
 
-import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import com.github.sandorw.mocabogaso.games.GameMove;
 import com.github.sandorw.mocabogaso.games.GameResult;
@@ -12,61 +12,35 @@ import com.google.common.collect.Lists;
 /**
  * Search tree of moves under consideration. Tracks the success of simulated games resulting from each
  * move via NodeResults created by the NodeResultsService that is passed in. Use the iterator method
- * to retrieve a SearchTreeIterator used to access SearchTreeNodes.
+ * to retrieve a SearchTreeIterator used to explore and operate on the tree.
  *
  * @author sandorw
  */
-public final class MonteCarloSearchTree<GM extends GameMove> {
+public final class MonteCarloSearchTree<GM extends GameMove, NR extends NodeResults> {
 	private volatile SearchTreeNode rootNode;
-	private volatile int MAX_NODE_DEPTH = 10;
     private volatile int NODE_EXPAND_THRESHOLD = 10;
     private volatile float EXPLORATION_CONSTANT = 1.0f;
-	private final NodeResultsService<? extends NodeResults> nodeResultsService;
+	private final NodeResultsService<NR> nodeResultsService;
 	
 	public <GS extends GameState<GM, ? extends GameResult>> 
-	        MonteCarloSearchTree(NodeResultsService<? extends NodeResults> nrService, GS initialGameState) {
+	        MonteCarloSearchTree(NodeResultsService<NR> nrService, GS initialGameState) {
 		rootNode = null;
 		nodeResultsService = nrService;
-		rootNode = new SearchTreeNode(null, 0, null, initialGameState);
+		rootNode = new SearchTreeNode(initialGameState);
 	}
 	
 	public GM getMostSimulatedMove() {
-		if ((rootNode == null) || !rootNode.expanded || rootNode.childNodes.isEmpty())
-			return null;
-		GM mostSimulatedMove = null;
-		int mostSimulations = -1;
-		for (SearchTreeNode childNode : rootNode.childNodes) {
-			int childSimulations = childNode.getNumSimulations();
-			if (childSimulations > mostSimulations) {
-				mostSimulations = childSimulations;
-				mostSimulatedMove = childNode.appliedMove;
-			}
-		}
-		return mostSimulatedMove;
+	    return rootNode.getMostSimulatedChildMove();
 	}
 	
 	public synchronized <GS extends GameState<GM, ? extends GameResult>> void advanceTree(GM move, GS resultingGameState) {
-		SearchTreeNode newRoot = null;
-		if (rootNode != null)
-			for (SearchTreeNode childNode : rootNode.childNodes)
-				if (childNode.getAppliedMove().equals(move)) {
-					newRoot = childNode;
-					newRoot.removeParentNode();
-					break;
-				}
-		if (newRoot == null)
-			newRoot = new SearchTreeNode(move, 0, null, resultingGameState);
-		rootNode = newRoot;		
-	}
-	
-	public synchronized SearchTreeIterator<GM> iterator() {
-		return new SearchTreeIterator<>(rootNode);
-    }
-	
-	public void setMaxNodeDepth(int maxDepth) {
-		if (maxDepth < 1)
-			throw new IllegalArgumentException();
-		MAX_NODE_DEPTH = maxDepth;
+	    SearchTreeNode newRoot = rootNode.findNodeWithMove(move);
+	    if (newRoot == null) {
+	        newRoot = new SearchTreeNode(resultingGameState);
+	    } else {
+	        newRoot.removeParentNodes();
+	    }
+	    rootNode = newRoot;
 	}
 	
 	public void setNodeExpandThreshold(int threshold) {
@@ -77,144 +51,176 @@ public final class MonteCarloSearchTree<GM extends GameMove> {
 		EXPLORATION_CONSTANT = explorationConstant;
 	}
 	
+	public SearchTreeIterator<GM,NR> iterator() {
+	    return new SearchTreeIterator<>(rootNode);
+	}
+	
 	/**
-	 * Individual node within the search tree. Contains the move to be applied at the node as well
-	 * as the NodeResults containing information about the success rate of the move. 
+	 * Individual node within the search tree. Contains NodeResults containing information about
+	 * the success rate of this state. Edges leading to other nodes include the move used to 
+	 * get there.
 	 *
 	 * @author sandorw
 	 */
 	public final class SearchTreeNode {
-		private final GM appliedMove;
-		private volatile SearchTreeNode parentNode;
+		private final List<SearchTreeNode> parentNodes;
 		private volatile boolean expanded;
-		private volatile List<SearchTreeNode> childNodes;
-		private final NodeResults nodeResults;
-        private final int nodeDepth;
-         
-        public <GS extends GameState<GM, ? extends GameResult>>
-        		SearchTreeNode(GM appliedMove, int depth, SearchTreeNode parent, GS resultingGameState) {
-        	this.appliedMove = appliedMove;
-        	parentNode = parent;
+		private volatile List<Pair<GM,SearchTreeNode>> childNodes;
+		private final NR nodeResults;
+		
+        private <GS extends GameState<GM, ? extends GameResult>>
+        		SearchTreeNode(GS resultingGameState) {
+            parentNodes = Lists.newArrayList();
         	expanded = false;
         	childNodes = Lists.newArrayList();
-        	nodeResults = nodeResultsService.getNewNodeResults(appliedMove, resultingGameState);
-        	nodeDepth = depth;
+        	nodeResults = nodeResultsService.getNewNodeResults(resultingGameState);
         }
         
-        public void applyGameResultFromSimulation(GameResult gameResult) {
-            nodeResults.applyGameResult(gameResult, appliedMove);
+        private void addParentNode(SearchTreeNode parent) {
+            parentNodes.add(parent);
         }
         
-        public float getNodeValue() {
-            return nodeResults.getValue() + getNodeExplorationValue();
+        private void removeParentNodes() {
+            parentNodes.clear();
         }
         
-        private float getNodeExplorationValue() {
-        	if (parentNode == null)
-                return 0.0f;
-            int parentSimulations = parentNode.getNumSimulations();
-            return EXPLORATION_CONSTANT*(float)Math.sqrt(Math.log(parentSimulations+1)/(getNumSimulations()+1));
+        private float getValue(String evaluatingPlayerName, int numParentSimulations) {
+            return nodeResults.getValue(evaluatingPlayerName) +
+                    getExplorationValue(numParentSimulations);
         }
 
-        public int getNumSimulations() {
+        private float getExplorationValue(int numParentSimulations) {
+            return EXPLORATION_CONSTANT*(float)Math.sqrt(Math.log(numParentSimulations+1)/getNumSimulations()+1);
+        }
+        
+        private int getNumSimulations() {
             return nodeResults.getNumSimulations();
         }
-        
-        public synchronized <GS extends GameState<GM, ? extends GameResult>> void expandNode(GS gameState) {
-        	if (!expanded && (getNodeDepth() < MAX_NODE_DEPTH) && !gameState.isGameOver() && 
-        			((getNumSimulations() >= NODE_EXPAND_THRESHOLD) || (getNodeDepth() == 0))) {
-        		for (GM move : gameState.getAllValidMoves()) {
-        			@SuppressWarnings("unchecked")
-					GS resultingGameState = (GS) gameState.getCopy();
-        			resultingGameState.applyMove(move);
-        			childNodes.add(new SearchTreeNode(move, nodeDepth+1, this, resultingGameState));
-        		}
-        		expanded = true;
-        	}
+
+        private synchronized <GS extends GameState<GM, ? extends GameResult>> void expandNode(GS gameState) {
+            if (!expanded && !gameState.isGameOver() &&
+                    ((getNumSimulations() >= NODE_EXPAND_THRESHOLD) || (this == rootNode))) {
+                for (GM move : gameState.getAllValidMoves()) {
+                    @SuppressWarnings("unchecked")
+                    GS resultingGameState = (GS) gameState.getCopy();
+                    resultingGameState.applyMove(move);
+                    SearchTreeNode childNode = new SearchTreeNode(resultingGameState);
+                    childNode.addParentNode(this);
+                    childNodes.add(Pair.of(move, childNode));
+                }
+                expanded = true;
+            }
         }
         
-        private int getNodeDepth() {
-            return nodeDepth - rootNode.nodeDepth;
+        private GM getMostSimulatedChildMove() {
+            if (!expanded)
+                return null;
+            GM mostSimulatedMove = null;
+            int mostSimulations = -1;
+            for (Pair<GM,SearchTreeNode> pair : childNodes) {
+                int numChildSimulations = pair.getRight().getNumSimulations();
+                if (numChildSimulations > mostSimulations) {
+                    mostSimulations = numChildSimulations;
+                    mostSimulatedMove = pair.getLeft();
+                }
+            }
+            return mostSimulatedMove;
         }
         
-        public GM getAppliedMove() {
-            return appliedMove;
+        private Pair<GM,SearchTreeNode> getHighestValueChildPair(String evaluatingPlayerName) {
+            if (!expanded)
+                return null;
+            int numParentSimulations = getNumSimulations();
+            Pair<GM,SearchTreeNode> bestPair = null;
+            float highestValue = Float.NEGATIVE_INFINITY;
+            for (Pair<GM,SearchTreeNode> pair : childNodes) {
+                float value = pair.getRight().getValue(evaluatingPlayerName, numParentSimulations);
+                if (value > highestValue) {
+                    highestValue = value;
+                    bestPair = pair;
+                }
+            }
+            return bestPair;
         }
         
-        public void removeParentNode() {
-            parentNode = null;
+        private SearchTreeNode findNodeWithMove(GM move) {
+            SearchTreeNode node = null;
+            for (Pair<GM,SearchTreeNode> pair : childNodes) {
+                if (move.equals(pair.getLeft())) {
+                    node = pair.getRight();
+                    break;
+                }
+            }
+            return node;
         }
+   
 	}
 	
 	/**
-	 * Iterator for moving through the search tree. Can move forward (deeper) and backwards (toward
-	 * the root node). When moving forward, the iterator moves to the child node with the highest
-	 * node value (the move that should be explored in the next simulated game).
-	 *
+	 * Iterator for passing through the search tree. Provides access to both parent and child nodes.
+	 * 
 	 * @author sandorw
 	 */
-	public static final class SearchTreeIterator<GM extends GameMove> implements Iterator<MonteCarloSearchTree<GM>.SearchTreeNode> {
-		private boolean atEnd;
-		private MonteCarloSearchTree<GM>.SearchTreeNode nextNode;
-		
-		//TODO: consider refactoring to allow iteration through arbitrary child nodes. AMAF will need this.
-		
-		public SearchTreeIterator(MonteCarloSearchTree<GM>.SearchTreeNode rootNode) {
-			atEnd = false;
-			nextNode = rootNode;
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return !atEnd;
-		}
-		
-		public boolean hasPrevious() {
-			if (atEnd)
-				return nextNode != null;
-			return nextNode.parentNode != null;
-		}
-		
-		@Override
-		public MonteCarloSearchTree<GM>.SearchTreeNode next() {
-			if (atEnd)
-				throw new NoSuchElementException();
-			MonteCarloSearchTree<GM>.SearchTreeNode returnNode = nextNode;
-			advanceToNextExplorationNode();
-			return returnNode;
-		}
-		
-		private void advanceToNextExplorationNode() {
-			if (!nextNode.expanded || nextNode.childNodes.isEmpty()) {
-				atEnd = true;
-				return;
-			}
-			MonteCarloSearchTree<GM>.SearchTreeNode explorationNode = null;
-			float bestNodeValue = Float.NEGATIVE_INFINITY;
-			for (MonteCarloSearchTree<GM>.SearchTreeNode childNode : nextNode.childNodes) {
-				float childNodeValue = childNode.getNodeValue();
-				if (childNodeValue > bestNodeValue) {
-					explorationNode = childNode;
-					bestNodeValue = childNodeValue;
-				}
-			}
-			nextNode = explorationNode;
-		}
-		
-		public MonteCarloSearchTree<GM>.SearchTreeNode previous() {
-			if (!hasPrevious())
-				throw new NoSuchElementException();
-			if (atEnd) {
-				atEnd = false;
-				return nextNode;
-			}
-			nextNode = nextNode.parentNode;
-			return nextNode;
-		}
-		
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
+	public static final class SearchTreeIterator<GM extends GameMove, NR extends NodeResults> {
+	    private MonteCarloSearchTree<GM,NR>.SearchTreeNode currentNode;
+	    private int currentChildIndex;
+	    private int currentParentIndex;
+	    
+	    public SearchTreeIterator(MonteCarloSearchTree<GM,NR>.SearchTreeNode startingNode) {
+	        currentNode = startingNode;
+	        resetIndices();
+	    }
+	    
+	    private void resetIndices() {
+	        currentChildIndex = -1;
+	        currentParentIndex = -1;
+	    }
+	    
+	    public boolean hasNext() {
+	        return ((currentNode.expanded) && (!currentNode.childNodes.isEmpty()));
+	    }
+	    
+	    public boolean hasNextChild() {
+	        return (currentChildIndex < currentNode.childNodes.size()-1);
+	    }
+	    
+	    public boolean hasNextParent() {
+	        return (currentParentIndex < currentNode.parentNodes.size()-1);
+	    }
+	    
+	    public GM advanceToNextExplorationNode(String evaluatingPlayerName) {
+	        Pair<GM,MonteCarloSearchTree<GM,NR>.SearchTreeNode> pair = currentNode.getHighestValueChildPair(evaluatingPlayerName);
+	        currentNode = pair.getRight();
+	        resetIndices();
+	        return pair.getLeft();
+	    }
+	    
+	    public <GS extends GameState<GM, ? extends GameResult>> void expandNode(GS gameState) {
+	        currentNode.expandNode(gameState);
+	    }
+	    
+	    public NR getCurrentNodeResults() {
+	        return currentNode.nodeResults;
+	    }
+	    
+	    public void advanceChildNode() {
+	        ++currentChildIndex;
+	    }
+	    
+	    public void advanceParentNode() {
+	        ++currentParentIndex;
+	    }
+	    
+	    public SearchTreeIterator<GM,NR> getCurrentChildIterator() {
+	        return new SearchTreeIterator<>(currentNode.childNodes.get(currentChildIndex).getRight());
+	    }
+	    
+	    public GM getCurrentChildMove() {
+	        return currentNode.childNodes.get(currentChildIndex).getLeft();
+	    }
+	    
+	    public SearchTreeIterator<GM,NR> getCurrentParentIterator() {
+	        return new SearchTreeIterator<>(currentNode.parentNodes.get(currentParentIndex));
+	    }
 	}
 }
